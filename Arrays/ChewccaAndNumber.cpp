44d55065fc83624e1c37a41badb3c4150527aabc
aman212yadav <31543527+aman212yadav@users.noreply.github.com>
2019-01-12 18:47:20 +0530

    Create ChewccaAndNumber.cpp

---
+++ Arrays/ChewccaAndNumber.cpp
@@ -0,0 +1,19 @@
+// problem link https://hack.codingblocks.com/practice/p/369/62
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    long long minNumber=0;
+    if(s[0]!='9')
+       minNumber=min(s[0]-'0',9-s[0]+'0');
+       else
+       minNumber=9;
+       for(int i=1;i<s.size();i++)
+         {
+             minNumber=minNumber*10+min(s[i]-'0',9-s[i]+'0');
+         }
+         cout<<minNumber<<endl;
+}
