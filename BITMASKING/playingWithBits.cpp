e9ec075f27167d0014399a5fcd15720e46205792
aman212yadav <31543527+aman212yadav@users.noreply.github.com>
2019-01-26 12:54:22 +0530

    Update playingWithBits.cpp

--- BITMASKING/playingWithBits.cpp
+++ BITMASKING/playingWithBits.cpp
@@ -1,1 +1,23 @@
 //  problem link https://hack.codingblocks.com/practice/p/366/102
+
+
+#include<iostream>
+using namespace std;
+int main() {
+  long long sum, q,a,b,i,k;
+  cin>>q;
+  while(q--){
+    cin>>a>>b;
+    sum=0;
+    for(i=a;i<=b;i++){
+      k=i;
+      while(k){
+        k=k&(k-1);
+        sum++;
+      }
+    }
+    cout<<sum<<"\n";
+    
+  }
+	return 0;
+}
