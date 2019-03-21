313635bbac9d7b32be6adeb1faef347e04e286bd
aman212yadav <31543527+aman212yadav@users.noreply.github.com>
2019-03-21 20:03:01 +0530

    Update strongestFighter.cpp

--- QUEUE/strongestFighter.cpp
+++ QUEUE/strongestFighter.cpp
@@ -4,26 +4,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-int n,i,k;
+long long n,i,k;
 cin>>n;
-int arr[n];
-queue<int> q;
+long long  arr[n];
+deque<int> q;
 for(i=0;i<n;i++)cin>>arr[i];
 cin>>k;
 for(i=0;i<k;i++){
-    if(!q.empty() && arr[i]>arr[q.front()])
-        q.pop();
-    q.push(i);
+    while(!q.empty() && arr[i]>arr[q.back()])
+        q.pop_back();
+    q.push_back(i);
 }
 cout<<arr[q.front()]<<" ";
 for(;i<n;i++){
-    while(!q.empty()&&(q.front()<=i-k || arr[i]>arr[q.front()]))
-        q.pop();
-    q.push(i);
+    while(!q.empty()&&q.front()<=i-k)
+        q.pop_front();
+    while(!q.empty() && arr[i]>arr[q.back()])
+      q.pop_back();
+    q.push_back(i);
     cout<<arr[q.front()]<<" ";
-}
-
-
-
-
-}
+}}
